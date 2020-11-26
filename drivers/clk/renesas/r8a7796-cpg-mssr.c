cd668deccf258560669743ac2fdaa8c0abbe8a66
Ronan Le Martret <ronan.lemartret@iot.bzh>
2020-11-26 15:09:06 +0000

    clk: add clock for MFIS registers
    Fix for M3ulcb
    
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- drivers/clk/renesas/r8a7796-cpg-mssr.c
+++ drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -141,6 +141,7 @@ static struct mssr_mod_clk r8a7796_mod_clks[] __initdata = {
 	DEF_MOD("msiof2",		 209,	R8A7796_CLK_MSO),
 	DEF_MOD("msiof1",		 210,	R8A7796_CLK_MSO),
 	DEF_MOD("msiof0",		 211,	R8A7796_CLK_MSO),
+	DEF_MOD("mfis",		         213,	R8A7796_CLK_MSO),
 	DEF_MOD("sys-dmac2",		 217,	R8A7796_CLK_S3D1),
 	DEF_MOD("sys-dmac1",		 218,	R8A7796_CLK_S3D1),
 	DEF_MOD("sys-dmac0",		 219,	R8A7796_CLK_S0D3),
